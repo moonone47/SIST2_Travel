56247f0ca9b258b86ce2373009afc9e14bfc5daf
MOON_ <moonj0522@gmail.com>
2021-07-27 19:08:16 +0900

    .

---
+++ SIST2_Travel/src/main/java/travel/community/event/Comment.java
@@ -0,0 +1,25 @@
+package travel.community.event;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/community/event/comment.do")
+public class Comment extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/community/event/comment.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+
+}
