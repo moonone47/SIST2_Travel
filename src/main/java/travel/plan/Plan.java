b501c62d5d37c7caf34b44c4e9d2b8c0e31dffc5
bangsubin97 <tnqls4691@naver.com>
2021-07-29 22:02:22 +0900

    .

--- src/main/java/travel/plan/Plan.java
+++ src/main/java/travel/plan/Plan.java
@@ -1,7 +1,5 @@
 package travel.plan;
 
-import travel.community.question.BoardDAO;
-import travel.community.question.BoardDTO;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,8 +24,44 @@ public class Plan extends HttpServlet {
 
 		req.setAttribute("citys",citys);
 
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/plan/planadd.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/plan/plan.jsp");
 		dispatcher.forward(req, resp);
 	}
 
 }
+
+
+
+
+//plan.java 시작 
+//plan.jsp 전체 일정을 세팅 (: 일정설정 완료)  
+//planinfo.java -> tblPlan에 전체 일정 생성 
+
+//->  todo: select(citydto에 정보 where절 날리기)으로 planseq 얻어오기 -> session에 담아두기
+
+//planinfo.java -> planadd.jsp -> 상세 일정 추가 
+//planadd.jsp (일정 하나 추가) -> planadd.java (placeDAO add() -> 일정 추가 -> planadd.java -> placeDAO.getList() -> planadd.jsp에 일정 리스트 뿌려줌)
+//리스트 일정 삭제 구현... x버튼 -> DB delete where 
+//순서 바꾸는 것: hidden tag 이용해서 sort
+ 
+//  -> (일정추가 버튼을 누르면) 상세 일정 하나씩 추가 완료 (: 임시테이블 tblPlan2에 담아놓음 )
+//planadd.java -> plandd.jsp -> (일정 완료를 누르면) 모든 일정을 DB에 저장 -> (tblPlan2(임시) -> 음식점/숙소/명소(저장소) 옮겨주기.. planseq 얻어오기) -> 끝?
+//	-> 임시 테이블에 있는 사용자의 레코드는 다 삭제 delete all where id = id;
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
