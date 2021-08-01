3aa09ca49256c2b27deb26cb0dbc27043ab91e8c
MOON_ <moonj0522@gmail.com>
2021-08-01 18:00:48 +0900

    .

--- src/main/java/travel/mypage/Editprofileok.java
+++ src/main/java/travel/mypage/Editprofileok.java
@@ -8,18 +8,52 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/mypage/editprofileok.do")
 public class Editprofileok extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		req.setCharacterEncoding("UTF-8");
+
+		String name = req.getParameter("name");
+		String nick = req.getParameter("nick");
+		String email = req.getParameter("email");
+		String phone = req.getParameter("phone");
+		String zip = req.getParameter("zip");
+		String address = req.getParameter("address");
+		String birthday = req.getParameter("birthday");
+		String gender = req.getParameter("gender");
+		String id = req.getParameter("id");
 		
-
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/mypage/editprofileok.jsp");
-		dispatcher.forward(req, resp);
-
+		MypageDAO dao = new MypageDAO();
+		MypageDTO dto = new MypageDTO();
+		
+		HttpSession session = req.getSession();
+		
+		dto.setId(id);
+		dto.setName(name);
+		dto.setNick(nick);
+		dto.setEmail(email);
+		dto.setPhone(phone);
+		dto.setZip(zip);
+		dto.setAddress(address);
+		dto.setBirthday(birthday);
+		dto.setGender(gender);
+		
+		int result = dao.edit(dto);
+		
+		if (result == 1) {
+			resp.sendRedirect("/index.do");
+		} else {
+			resp.sendRedirect("/mypage/editprofile.do");
+		}
+		
 	}
-
 }
+
+
+
+
--- src/main/webapp/inc/asset.jsp
+++ src/main/webapp/inc/asset.jsp
@@ -7,6 +7,5 @@
 <link rel="stylesheet" href="<%= request.getContextPath() %>/asset/css/bootstrap.css" />
 <script src="<%= request.getContextPath() %>/asset/js/jquery-3.6.0.js"></script>
 <script src="<%= request.getContextPath() %>/asset/js/bootstrap.js"></script>
-<%--헤더 css--%>
-<link rel="stylesheet" href="<%= request.getContextPath() %>/asset/css/style.css">
+
 
