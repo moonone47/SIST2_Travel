e530c45d607cff1aaa041c8b2b8266514596801c
HL <kmr345@naver.com>
2021-08-02 15:55:37 +0900

    회원가입 95% 완료

--- src/main/java/travel/community/event/EditOk.java
+++ src/main/java/travel/community/event/EditOk.java
@@ -1,21 +1,39 @@
 package travel.community.event;
 
-import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/community/editok.do")
+@WebServlet("/community/event/editok.do")
 public class EditOk extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		
+		CheckMember cm = new CheckMember();
+		cm.check(req, resp);
+
+
+		//할일
+		//1. 데이터 가져오기(seq)
+		//2. DB 작업 > DAO 위임 > select where seq
+		//3. BoardDTO 반환 > JSP 호출하기 + 전달하기
+
+		//1.
+		String seq = req.getParameter("seq");
+
+		//2.
+		EventDAO dao = new EventDAO();
+
+		EventDTO dto = dao.get(seq);
+
+
+		//3.
+		req.setAttribute("dto", dto);
 
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/community/editok.jsp");
 		dispatcher.forward(req, resp);
