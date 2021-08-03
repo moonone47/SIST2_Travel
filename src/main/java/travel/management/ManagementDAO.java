811d4d84ede8b52eded088b3f2a5a142d70fe951
MOON_ <moonj0522@gmail.com>
2021-08-03 17:48:48 +0900

    여행관리 중간 수정본

--- src/main/java/travel/management/ManagementDAO.java
+++ src/main/java/travel/management/ManagementDAO.java
@@ -1,5 +1,59 @@
 package travel.management;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import travel.DBUtil;
+import travel.mypage.TravelPlanDTO;
+
 public class ManagementDAO {
 
+	private Connection conn;
+	private Statement stat;
+	private PreparedStatement pstat;
+	private ResultSet rs;
+	
+	
+	public ManagementDAO() {
+		try {
+			conn = DBUtil.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public ArrayList<ManagementDTO> getlist(String id) {
+		try {
+		
+			String sql = "select * from vwTravelPlan where id=" + id;
+		
+				stat = conn.createStatement();
+				rs = stat.executeQuery(sql);
+				
+				ArrayList<ManagementDTO> list = new ArrayList<ManagementDTO>();
+				while(rs.next()) {
+					
+					ManagementDTO dto = new ManagementDTO();
+					dto.setName(rs.getString("name"));
+					dto.setCityname(rs.getString("cityname"));
+					dto.setDaystarttravel(rs.getString("daystarttravel"));
+					dto.setDayendtravel(rs.getString("dayendtravel"));
+					dto.setWish(rs.getString("wish"));
+					dto.setPlanseq(rs.getString("planseq"));
+					
+					list.add(dto);
+				}
+				return list;
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;	
+	
+	
+	}
 }
--- src/main/java/travel/management/TravelPlan.java
+++ src/main/java/travel/management/TravelPlan.java
@@ -1,6 +1,7 @@
 package travel.management;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/management/travelplan.do")
 public class TravelPlan extends HttpServlet {
@@ -15,7 +17,25 @@ public class TravelPlan extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		// 여행관리에 여행 내역 전체 조회 
 		
+		HttpSession session = req.getSession();
+		String id = (String)session.getAttribute("id");
+		
+		ManagementDAO dao = new ManagementDAO();
+		
+		ArrayList<ManagementDTO> list = dao.getlist("3"); // 3에 id 넣자	
+			
+							
+		for(ManagementDTO dto: list) {			
+			
+			dto.setDaystarttravel(dto.getDaystarttravel().substring(0, 11));
+			dto.setDayendtravel(dto.getDayendtravel().substring(0, 11));
+		
+		}
+		
+		req.setAttribute("list", list);
+
 
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/management/travelplan.jsp");
 		dispatcher.forward(req, resp);
