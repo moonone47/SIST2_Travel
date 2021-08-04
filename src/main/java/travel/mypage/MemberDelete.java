61d20c0df54c0a468c880a29b40b51f6e531acc1
bangsubin97 <tnqls4691@naver.com>
2021-08-04 19:50:15 +0900

    .

--- src/main/java/travel/mypage/MemberDelete.java
+++ src/main/java/travel/mypage/MemberDelete.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+//
 @WebServlet("/mypage/memberdelete.do")
 public class MemberDelete extends HttpServlet {
 
