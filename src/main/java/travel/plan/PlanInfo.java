8ca78d4ee61afd04bc852ca9ac41adccc3d6e0ce
HL <kmr345@naver.com>
2021-08-04 11:41:59 +0900

    temp

--- src/main/java/travel/plan/PlanInfo.java
+++ src/main/java/travel/plan/PlanInfo.java
@@ -1,19 +1,12 @@
 package travel.plan;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet("/plan/planinfo.do")
 public class PlanInfo extends HttpServlet {
@@ -51,8 +44,8 @@ public class PlanInfo extends HttpServlet {
 		HttpSession session = req.getSession();
 		session.setAttribute("planseq", planseq);
 
-		
-		resp.sendRedirect("/SIST2_Travel/plan/between.do?planseq=" + planseq);
+
+		resp.sendRedirect("/SIST2_Travel/plan/between.do?planseq=" + planseq+"&isedit=n");
 
 	}
 
